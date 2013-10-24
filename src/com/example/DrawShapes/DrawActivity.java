e4e0a1826bae125a0f7cdd76a90e43081a86fde3
Yngvi Bjornsson <yngvi.bjornsson@gmail.com>
2013-10-24 17:55:41 +0000

    Empty project

---
+++ src/com/example/DrawShapes/DrawActivity.java
@@ -0,0 +1,18 @@
+package com.example.DrawShapes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yngvi
+ * Date: 24.10.2013
+ * Time: 17:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class DrawActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.draw);
+    }
+}
---
+++ src/com/example/DrawShapes/DrawView.java
@@ -0,0 +1,21 @@
+package com.example.DrawShapes;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yngvi
+ * Date: 24.10.2013
+ * Time: 17:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class DrawView extends View {
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
